9479e8b67ac61e1f54466f88512a60ee21b405fb
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-12-18 19:35:50 +0530

    Basic Layout Created

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import card1 from "../assets/card1.png"
 
 export class Card1 extends Component {
   render() {
     return (
-      <div class="card" style={{margin:"80px 50px"}}>
-        <div class="card-body">This is some text within a card body.</div>
+      <div class="" style={{ margin: "80px 50px"}}>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-6">
+                <div class="card-body">
+                  <h5 class="card-title"><b>The easiest Video maker for social media marketing</b></h5>
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi hic tempora labore, dolorem quos, inventore cupiditate, ipsa officia illo hic tempora labore
+                  </p>
+                  <a href="#" class="btn badge-pill badge-primary" style={{backgroundColor:"#3498eb"}}>
+                    Sign up free
+                  </a>
+              </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card-body">
+                  <h5 class="card-title" style={{color:"#04c289"}}>Videos <strong>In Minutes</strong></h5>
+                  <p class="card-text">
+                    <img src={card1} />
+                  </p>
+                </div>
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import card2 from "../assets/card2.png"
 
 export class Card2 extends Component {
   render() {
     return (
-      <div class="card" style={{ margin: "80px 50px" }}>
-        <div class="card-body">This is some text within a card body.</div>
+      <div class="card" style={{ margin: "80px 50px",backgroundColor:"#f7f7f7"  }}>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-6">
+                <div class="card-body">
+                  <p class="card-text">
+                    <img src={card2} />
+                  </p>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card-body">
+                  <h5 class="card-title"><b>Turn content / link into video -</b></h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <a href="#" class="btn badge-pill badge-primary" style={{backgroundColor:"#3498eb"}}>
+                    Get started for free
+                  </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
