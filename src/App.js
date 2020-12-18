2020b2cf7e124536ccbe6e3c42dc6234995d70d6
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-12-18 18:20:42 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      My Page Here
+      <div className="jumbotron">Something</div>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
